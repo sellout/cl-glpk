78527d39b66eccb7d5970d15248e0c4fa1543eb8
kai.kaminski <kai.kaminski@gmx.de>
2007-05-16 11:23:57 -0400

    Initial import.
    darcs-hash:20070516152357-52157-2bd4769eb2817b2c8cb85d72dd8494f6fc6a310a.gz

---
+++ examples/sample.lisp
@@ -0,0 +1,76 @@
+;;; Copyright (c) 2007, Kai Kaminski.  All rights reserved.
+
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+
+;;;   * Redistributions of source code must retain the above copyright
+;;;     notice, this list of conditions and the following disclaimer.
+
+;;;   * Redistributions in binary form must reproduce the above
+;;;     copyright notice, this list of conditions and the following
+;;;     disclaimer in the documentation and/or other materials
+;;;     provided with the distribution.
+
+;;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR 'AS IS' AND ANY EXPRESSED
+;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;; This file corresponds to EXAMPLES/SAMPLE.C in the GLPK distribution.
+
+;;; This problem is described in Section 1.3 of the GLPK manual.
+
+(in-package #:cl-glpk)
+
+(defun sample ()
+  "An implementation using the FFI bindings directly."
+  (let ((lp (%create-prob))
+	(is (foreign-alloc :int :count 10))
+	(js (foreign-alloc :int :count 10))
+	(as (foreign-alloc :double :count 10)))
+    (%set-prob-name lp "sample")
+    (%set-obj-dir lp :max)
+
+    (%add-rows lp 3)
+    (%set-row-name lp 1 "p")
+    (%set-row-bnds lp 1 :upper-bound 0d0 100d0)
+    (%set-row-name lp 2 "q")
+    (%set-row-bnds lp 2 :upper-bound 0d0 600d0)
+    (%set-row-name lp 3 "r")
+    (%set-row-bnds lp 3 :upper-bound 0d0 300d0)
+
+    (%add-cols lp 3)
+    (%set-col-name lp 1 "x1")
+    (%set-col-bnds lp 1 :lower-bound 0d0 0d0)
+    (%set-obj-coef lp 1 10d0)
+    (%set-col-name lp 2 "x2")
+    (%set-col-bnds lp 2 :lower-bound 0d0 0d0)
+    (%set-obj-coef lp 2 6d0)
+    (%set-col-name lp 3 "x3")
+    (%set-col-bnds lp 3 :lower-bound 0d0 0d0)
+    (%set-obj-coef lp 3 4d0)
+
+    (iter (for k from 1)
+	  (for a in '(1d0 1d0 1d0 10d0 4d0 5d0 2d0 2d0 6d0))
+	  (setf (mem-aref is :int k) (ceiling k 3))
+	  (setf (mem-aref js :int k) (1+ (mod (1- k) 3)))
+	  (setf (mem-aref as :double k) a))
+
+    (%load-matrix lp 9 is js as)
+    (%simplex lp)
+
+    (format t "z = ~a; x1 = ~a; x2 = ~a; x3 = ~a~%"
+	    (%get-obj-val lp)
+	    (%get-col-prim lp 1)
+	    (%get-col-prim lp 2)
+	    (%get-col-prim lp 3))
+
+    (%delete-prob lp)))
